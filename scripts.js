83083e65907f402a7b0d8931bc79831c1c1ae70a
tinamayswinkels <tinamayswinkels@users.noreply.github.com>
2021-07-18 18:46:01 +0800

    gallery page

--- scripts.js
+++ scripts.js
@@ -15,3 +15,15 @@ $( document ).ready(function() {
       });
 
 });
+
+
+lightGallery(document.getElementById('lightgallery'), {
+  speed: 500,
+  download: false,
+});
+
+lightGallery(document.getElementById('lightgallery-2'), {
+  speed: 500,
+  download: false,
+  allowMediaOverlap: false,
+});
